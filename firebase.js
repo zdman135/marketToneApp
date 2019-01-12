85a4a3994c9d62ed1cfc5b98b6fc2def32982f84
VivaMango <vivamangomail@gmail.com>
2019-01-12 11:05:39 -0800

    npm installed firebase, successfully recieving response from intrino server, next step configure express to sendFile our HTML to live server

--- firebase.js
+++ firebase.js
@@ -2,19 +2,52 @@
 console.log("testing our file link")
 
 
-var firebaseConfig = {
-    apiKey: "AIzaSyBBXXCcOQHTChGULdlDwY4JK7545B_A1XI",
-    authDomain: "marketwatchapp-35777.firebaseapp.com",
-    databaseURL: "https://marketwatchapp-35777.firebaseio.com",
-    projectId: "marketwatchapp-35777",
-    storageBucket: "marketwatchapp-35777.appspot.com",
-    messagingSenderId: "593066949760"
-  };
-
-  firebase.initializeApp(firebaseConfig);
+var firebase = require('firebase');
+var app = firebase.initializeApp({
+  apiKey: "AIzaSyBBXXCcOQHTChGULdlDwY4JK7545B_A1XI",
+  authDomain: "marketwatchapp-35777.firebaseapp.com",
+  databaseURL: "https://marketwatchapp-35777.firebaseio.com",
+  projectId: "marketwatchapp-35777",
+  storageBucket: "marketwatchapp-35777.appspot.com",
+  messagingSenderId: "593066949760"
+});
 
   var database = firebase.database()
 
   database.ref().on("child_added" , function(childSnapshot) {
     console.log(childSnapshot , "childSnapshot")
   });
+  
+
+  // NODE CONFIGURE CODE
+
+  const http = require('http');
+
+  const hostname = '127.0.0.1';
+  const port = 3000;
+  
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    // res.end('Hello World\n');
+  });
+  
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+
+
+// INTRINO SDK CODE
+
+var intrinioSDK = require('intrinio-sdk');
+const util = require('util')
+ 
+intrinioSDK.ApiClient.instance.authentications['ApiKeyAuth'].apiKey = "OjYwYjU0ZGM3NWM3MTFmYTMwZGU1NTMzYTNjMjk1NjZl";
+ 
+var companyAPI = new intrinioSDK.CompanyApi()
+ 
+companyAPI.getAllCompanies().then(function(data) {
+  console.log(util.inspect(data, false, null, true));
+}, function(error) {
+  console.error(error);
+});
