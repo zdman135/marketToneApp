c8fee264bfb5e31d2e0bdd9af0ac79a963ade4e5
VivaMango <vivamangomail@gmail.com>
2019-01-15 17:00:03 -0800

    preparing for merge with app.js and testing

--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@
 console.log("testing our file link")
 
 
-var firebaseConfig = {
+var userSearchDatabaseConfig = {
     apiKey: "AIzaSyBBXXCcOQHTChGULdlDwY4JK7545B_A1XI",
     authDomain: "marketwatchapp-35777.firebaseapp.com",
     databaseURL: "https://marketwatchapp-35777.firebaseio.com",
@@ -11,40 +11,39 @@ var firebaseConfig = {
     messagingSenderId: "593066949760"
   };
 
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(userSearchDatabaseConfig);
 
-  var database = firebase.database()
+  var database = firebase.database();
 
+  var testPush = database.ref().push({
+    test: "test data"
+  })
 
+  var pushKey = testPush.getKey()
+  console.log(pushKey , "pushKey")
 
   database.ref().on("child_added" , function(childSnapshot) {
-    console.log(childSnapshot , "childSnapshot")
+    console.log(childSnapshot.val().test , "childsnapshot.test")
+
   });
 
 
   // Pushing relevant fields from newsAPI response to Firebase
   function firebaseNewsPush(newsResponse) {
+    console.log(newsResponse , "newsResponse inside firebaseNewsPush function")
     database.ref().push({
-      title: newsResponse.URL,
-      URL: newsResponse.URL,
-      description: newsResponse.Description,
+      title: newsResponse.url,
+      URL: newsResponse.url,
+      description: newsResponse.description,
 
     })
   }
 
 // Pushing relevant fields from Watson Tone Analyzer respnse to Firebase
   function firebaseWatsonPush(watsonResponse) {
+    console.log(watsonResponse , "watsonResponse inside firebaseWatsonPush function")
     database.ref().push({
-      tonesObject: watsonResponse.Tones
+      tonesObject: watsonResponse.document_tone
     })
   }
-
-
-  // function firebaseAlphaPush(alphaResponse) {
-  //   database.ref().push({
-  //     symbol: alphaResponse[ "Global Quote" ][ "01. symbol"],
-  //     changePercent: alphaResponse[ "Global Quote" ][ "10. change percent"],
-  //   })
-
-  // }
 
